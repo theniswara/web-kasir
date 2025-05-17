a78734f6338afd0d8ed56ccf4312925baa885d15
theniswara <daniswara.mdr@proton.me>
2025-05-17 13:39:24 +0700

    Implement code changes to enhance functionality and improve performance

---
+++ includes/scripts.php
@@ -0,0 +1,11 @@
+
+<!-- SweetAlert -->
+<script src="js/sweetalert2@11"></script>
+<script>
+  Swal.fire({
+  title: 'Error!',
+  text: 'Do you want to continue',
+  icon: 'error',
+  confirmButtonText: 'Cool'
+})
+</script>
