612e4ffe74b922ae1c942352671db2773bcad01f
theniswara <daniswara.mdr@proton.me>
2025-05-31 12:59:19 +0700

    Refactor sidebar and registration page for improved structure and readability

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,121 +1,127 @@
-        <!-- Menu -->
+<!-- Menu -->
 
-        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-          <div class="app-brand demo">
-            <a href="index.php" class="app-brand-link">
-              <span class="app-brand-logo demo">
-                <span class="text-primary">
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-                      <g fill="currentColor">
-                      <path d="M1 0a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h5v1H2v2h6V9H2V2h10v4h2V1a1 1 0 0 0-1-1z" />
-                      <path d="M10 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1zm1 7V9h3v5z" />
-                      </g>
-                  </svg>
-                </span>
-              </span>
-              <span class="app-brand-text demo menu-text fw-bold ms-2">GadgetIn</span>
-            </a>
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+  <div class="app-brand demo">
+    <a href="index.php" class="app-brand-link">
+      <span class="app-brand-logo demo">
+        <span class="text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+            <g fill="currentColor">
+              <path d="M1 0a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h5v1H2v2h6V9H2V2h10v4h2V1a1 1 0 0 0-1-1z" />
+              <path d="M10 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1zm1 7V9h3v5z" />
+            </g>
+          </svg>
+        </span>
+      </span>
+      <span class="app-brand-text demo menu-text fw-bold ms-2">GadgetIn</span>
+    </a>
 
-            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
-              <i class="bx bx-chevron-left d-block d-xl-none align-middle"></i>
-            </a>
-          </div>
+    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
+      <i class="bx bx-chevron-left d-block d-xl-none align-middle"></i>
+    </a>
+  </div>
 
-          <div class="menu-divider mt-0"></div>
+  <div class="menu-divider mt-0"></div>
 
-          <div class="menu-inner-shadow"></div>
+  <div class="menu-inner-shadow"></div>
 
-          <ul class="menu-inner py-1">
-            <li class="menu-header small text-uppercase">
-              <span class="menu-header-text">Core</span>
-            </li>
-            <!-- Dashboards -->
-            <li class="menu-item active">
-              <a href="index.php" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home"></i>
-                <div class="text-truncate" data-i18n="Dashboards">Dashboards</div>
-              </a>
-            </li>
-            <li class="menu-item">
-              <a href="buat-pesanan.php" class="menu-link">
-                <i class="menu-icon tf-icons bx bxs-create"></i>
-                <div class="text-truncate" data-i18n="Dashboards">Buat Pesanan</div>
-              </a>
-            </li>
+  <ul class="menu-inner py-1">
 
-            <li class="menu-item">
-              <a href="pesanan.php" class="menu-link">
-                <i class="menu-icon tf-icons bx bxs-orders"></i>
-                <div class="text-truncate" data-i18n="Dashboards">Pesanan</div>
-              </a>
-            </li>
+    <li class="menu-header small text-uppercase">
+      <span class="menu-header-text">Core</span>
+    </li>
 
-              <!-- Interface -->
-            <li class="menu-header small text-uppercase">
-              <span class="menu-header-text">Interface</span>
-            </li>
-            <!-- Pages -->
-            <li class="menu-item">
-              <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bxs-products"></i>
-                <div class="text-truncate" data-i18n="Account Settings">Produk</div>
-              </a>
-              <ul class="menu-sub">
-                <li class="menu-item">
-                  <a href="tambah-produk.php" class="menu-link">
-                    <div class="text-truncate" data-i18n="AddCategories">Tambah Produk</div>
-                  </a>
-                </li>
-                <li class="menu-item">
-                  <a href="list-produk.php" class="menu-link">
-                    <div class="text-truncate" data-i18n="Notifications">List Produk</div>
-                  </a>
-                </li>
-              </ul>
-            </li>
+    <!-- Dashboards -->
+    <li class="menu-item">
+      <a href="index.php" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-home"></i>
+        <div class="text-truncate" data-i18n="Dashboards">Dashboards</div>
+      </a>
+    </li>
 
-            <li class="menu-header small text-uppercase">
-              <span class="menu-header-text">Interface</span>
-            </li>
-            <!-- Pages -->
-            <li class="menu-item">
-              <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bxs-customer"></i>
-                <div class="text-truncate" data-i18n="Account Settings">Pelanggan</div>
-              </a>
-              <ul class="menu-sub">
-                <li class="menu-item">
-                  <a href="tambah-pelanggan.php" class="menu-link">
-                    <div class="text-truncate" data-i18n="AddCategories">Tambah Pelanggan</div>
-                  </a>
-                </li>
-                <li class="menu-item">
-                  <a href="list-pelanggan.php" class="menu-link">
-                    <div class="text-truncate" data-i18n="Notifications">List Pelanggan</div>
-                  </a>
-                </li>
-              </ul>
-            </li>
+    <li class="menu-item">
+      <a href="buat-pesanan.php" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-cart"></i>
+        <div class="text-truncate" data-i18n="Dashboards">Buat Pesanan</div>
+      </a>
+    </li>
 
-             <li class="menu-item">
-              <a href="javascript:void(0);" class="menu-link menu-toggle">
-                <i class="menu-icon tf-icons bx bxs-admin"></i>
-                <div class="text-truncate" data-i18n="Account Settings">Admin</div>
-              </a>
-              <ul class="menu-sub">
-                <li class="menu-item">
-                  <a href="tambah-admin.php" class="menu-link">
-                    <div class="text-truncate" data-i18n="AddCategories">Tambah Admin</div>
-                  </a>
-                </li>
-                <li class="menu-item">
-                  <a href="list-admin.php" class="menu-link">
-                    <div class="text-truncate" data-i18n="Notifications">List Admin</div>
-                  </a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </aside>
-        <!-- / Menu -->
+    <li class="menu-item">
+      <a href="pesanan.php" class="menu-link">
+        <i class="menu-icon tf-icons bx bxs-orders"></i>
+        <div class="text-truncate" data-i18n="Dashboards">Daftar Pesanan</div>
+      </a>
+    </li>
+
+    <!-- Interface -->
+    <li class="menu-header small text-uppercase">
+      <span class="menu-header-text">Master Data</span>
+    </li>
+    <!-- Pages -->
+    <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bxs-products"></i>
+        <div class="text-truncate" data-i18n="Account Settings">Produk</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="tambah-produk.php" class="menu-link">
+            <div class="text-truncate" data-i18n="AddCategories">Tambah Produk</div>
+          </a>
+        </li>
+        <li class="menu-item">
+          <a href="list-produk.php" class="menu-link">
+            <div class="text-truncate" data-i18n="Notifications">List Produk</div>
+          </a>
+        </li>
+      </ul>
+    </li>
+
+    <!-- Pages -->
+    <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bxs-customer"></i>
+        <div class="text-truncate" data-i18n="Account Settings">Pelanggan</div>
+      </a>
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="tambah-pelanggan.php" class="menu-link">
+            <div class="text-truncate" data-i18n="AddCategories">Tambah Pelanggan</div>
+          </a>
+        </li>
+        <li class="menu-item">
+          <a href="list-pelanggan.php" class="menu-link">
+            <div class="text-truncate" data-i18n="Notifications">List Pelanggan</div>
+          </a>
+        </li>
+      </ul>
+    </li>
+
+    <li class="menu-header small text-uppercase">
+      <span class="menu-header-text">Admin</span>
+    </li>
+    <li class="menu-item">
+      <a href="list-admin.php" class="menu-link">
+        <i class="menu-icon tf-icons bx bxs-admin"></i>
+        <div class="text-truncate" data-i18n="Account Settings">Admin</div>
+      </a>
+    </li>
+  </ul>
+</aside>
+<!-- / Menu -->
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    var current = window.location.pathname.split('/').pop();
+    if (current === '' || current === 'admin') current = 'index.php';
+    document.querySelectorAll('.menu-link').forEach(function(link) {
+      var href = link.getAttribute('href');
+      if (href === current) {
+        var parentMenuItem = link.closest('.menu-item');
+        if (parentMenuItem) {
+          parentMenuItem.classList.add('active');
+        }
+      }
+    });
+  });
+</script>
--- registrasi.php
+++ registrasi.php
@@ -1,22 +1,19 @@
-<?php 
+<?php
 
 require 'config/auth.php';
 
 # Kalau tombol register ditekan
-if(isset($_POST["register"])) {
+if (isset($_POST["register"])) {
   // Jalankan function
   // Kalau nilai > 0 = ada user baru yang masuk ke db
-  if(registrasi($_POST) > 0 ) {
+  if (registrasi($_POST) > 0) {
     // Kasih pop up
     echo "<script>
             alert('User baru berhasil ditambahkan, silahkan Login!')
           </script>";
-
- 
-  }else {
+  } else {
     echo mysqli_error($conn);
   }
-
 }
 
 
@@ -30,168 +27,165 @@ if(isset($_POST["register"])) {
   class="layout-wide customizer-hide"
   data-assets-path="admin/assets/"
   data-template="vertical-menu-template-free">
-  <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Registrasi</title>
+<head>
+  <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <meta name="description" content="" />
+  <title>Registrasi</title>
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="admin/assets/img/favicon/favicon.ico" />
+  <meta name="description" content="" />
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet" />
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="admin/assets/img/favicon/favicon.ico" />
 
-    <link rel="stylesheet" href="admin/assets/vendor/fonts/iconify-icons.css" />
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet" />
 
-    <!-- Core CSS -->
-    <!-- build:css assets/vendor/css/theme.css  -->
+  <link rel="stylesheet" href="admin/assets/vendor/fonts/iconify-icons.css" />
 
-    <link rel="stylesheet" href="admin/assets/vendor/css/core.css" />
-    <link rel="stylesheet" href="admin/assets/css/demo.css" />
+  <!-- Core CSS -->
+  <!-- build:css assets/vendor/css/theme.css  -->
 
-    <!-- Vendors CSS -->
+  <link rel="stylesheet" href="admin/assets/vendor/css/core.css" />
+  <link rel="stylesheet" href="admin/assets/css/demo.css" />
 
-    <link rel="stylesheet" href="admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+  <!-- Vendors CSS -->
 
-    <!-- endbuild -->
+  <link rel="stylesheet" href="admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-    <!-- Page CSS -->
-    <!-- Page -->
-    <link rel="stylesheet" href="admin/assets/vendor/css/pages/page-auth.css" />
+  <!-- endbuild -->
 
-    <!-- Color changes -->
-     <link rel="stylesheet" href="admin/assets/css/custom-overrides.css" />
-<link rel="stylesheet" href="admin/assets/css/sidebar-highlight-overrides.css" />
+  <!-- Page CSS -->
+  <!-- Page -->
+  <link rel="stylesheet" href="admin/assets/vendor/css/pages/page-auth.css" />
 
-    <!-- Helpers -->
-    <script src="admin/assets/vendor/js/helpers.js"></script>
-    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+  <!-- Color changes -->
+  <link rel="stylesheet" href="admin/assets/css/custom-overrides.css" />
+  <link rel="stylesheet" href="admin/assets/css/sidebar-highlight-overrides.css" />
 
-    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+  <!-- Helpers -->
+  <script src="admin/assets/vendor/js/helpers.js"></script>
+  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
 
-    <script src="admin/assets/js/config.js"></script>
-  </head>
+  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
 
-  <body>
+  <script src="admin/assets/js/config.js"></script>
+</head>
 
-<!-- Content -->
+<body>
 
-<div class="container-xxl">
-<div class="authentication-wrapper authentication-basic container-p-y">
-<div class="authentication-inner">
-  <!-- Register -->
-  <div class="card px-sm-6 px-0">
-    <div class="card-body">
-      <!-- Logo -->
-      <div class="app-brand justify-content-center">
-        <a href="index.html" class="app-brand-link gap-2">
-          <span class="app-brand-logo demo">
-            <span class="text-primary">
-            <svg
+  <!-- Content -->
+
+  <div class="container-xxl">
+    <div class="authentication-wrapper authentication-basic container-p-y">
+      <div class="authentication-inner">
+        <!-- Register -->
+        <div class="card px-sm-6 px-0">
+          <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center">
+              <a href="index.html" class="app-brand-link gap-2">
+                <span class="app-brand-logo demo">
+                  <span class="text-primary">
+                    <svg
                       xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
                       <g fill="currentColor">
-                      <path d="M1 0a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h5v1H2v2h6V9H2V2h10v4h2V1a1 1 0 0 0-1-1z" />
-                      <path d="M10 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1zm1 7V9h3v5z" />
+                        <path d="M1 0a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h5v1H2v2h6V9H2V2h10v4h2V1a1 1 0 0 0-1-1z" />
+                        <path d="M10 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1zm1 7V9h3v5z" />
                       </g>
-                  </svg>
-            </span>
-          </span>
-          <span class="app-brand-text demo text-heading fw-bold">GadgetIn</span>
-        </a>
+                    </svg>
+                  </span>
+                </span>
+                <span class="app-brand-text demo text-heading fw-bold">GadgetIn</span>
+              </a>
+            </div>
+            <!-- /Logo -->
+            <h3 class="mb-1">Registrasi</h3>
+            <p class="mb-6">Silahkan isi data diri anda untuk melakukan registrasi akun baru.</p>
+
+            <form id="formAuthentication" class="mb-6" action="" method="post">
+              <div class="mb-6">
+                <label for="username" class="form-label"> Username</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="username"
+                  name="username"
+                  placeholder="Masukkan username"
+                  autofocus
+                  required />
+
+              </div>
+              <div class="mb-6">
+                <label for="email" class="form-label"> Email</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email"
+                  name="email"
+                  placeholder="Masukkan email"
+                  autofocus
+                  required />
+
+              </div>
+              <div class="mb-6 form-password-toggle">
+                <label class="form-label" for="password">Password</label>
+                <div class="input-group input-group-merge">
+                  <input
+                    type="password"
+                    id="password"
+                    class="form-control"
+                    name="password"
+                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                    aria-describedby="password"
+                    required />
+                  <span class="input-group-text cursor-pointer"><i class="icon-base bx bx-hide"></i></span>
+                </div>
+              </div>
+              <div class="mb-6 form-password-toggle">
+                <label class="form-label" for="password2">Konfirmasi Password</label>
+                <div class="input-group input-group-merge">
+                  <input
+                    type="password"
+                    id="password2"
+                    class="form-control"
+                    name="password2"
+                    placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" 
+                    aria-describedby="password"
+                    required />
+                  <span class="input-group-text cursor-pointer"><i class="icon-base bx bx-hide"></i></span>
+                </div>
+              </div>
+              <div class="mb-6">
+                <button class="btn btn-primary d-grid w-100" type="submit" name="register">Registrasi</button>
+              </div>
+            </form>
+
+            <p class="text-center">
+              <span>Sudah punya akun?</span>
+              <a href="login.php">
+                <span class="text-primary fw-bold"> Login</span>
+              </a>
+            </p>
+
+          </div>
+        </div>
+        <!-- /Register -->
       </div>
-      <!-- /Logo -->
-      <h3 class="mb-1">Registrasi</h3>
-      <p class="mb-6">Silahkan isi data diri anda untuk melakukan registrasi akun baru.</p>
-
-      <form id="formAuthentication" class="mb-6" action="" method="post">
-        <div class="mb-6">
-          <label for="username" class="form-label"> Username</label>
-          <input
-            type="text"
-            class="form-control"
-            id="username"
-            name="username"
-            placeholder="Masukkan username"
-            autofocus 
-            required
-            />
-            
-        </div>
-                <div class="mb-6">
-          <label for="email" class="form-label"> Email</label>
-          <input
-            type="email"
-            class="form-control"
-            id="email"
-            name="email"
-            placeholder="Masukkan email"
-            autofocus 
-            required
-            />
-            
-        </div>
-        <div class="mb-6 form-password-toggle">
-          <label class="form-label" for="password">Password</label>
-          <div class="input-group input-group-merge">
-            <input
-              type="password"
-              id="password"
-              class="form-control"
-              name="password"
-              placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-              aria-describedby="password"
-              required
-              />
-            <span class="input-group-text cursor-pointer"><i class="icon-base bx bx-hide"></i></span>
-          </div>
-        </div>
-        <div class="mb-6 form-password-toggle">
-          <label class="form-label" for="password2">Konfirmasi Password</label>
-          <div class="input-group input-group-merge">
-            <input
-              type="password"
-              id="password2"
-              class="form-control"
-              name="password2"
-              placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
-              aria-describedby="password" 
-              required
-              />
-            <span class="input-group-text cursor-pointer"><i class="icon-base bx bx-hide"></i></span>
-          </div>
-        </div>
-        <div class="mb-6">
-          <button class="btn btn-primary d-grid w-100" type="submit" name="register">Registrasi</button>
-        </div>
-      </form>
-
-      <p class="text-center">
-        <span>Sudah punya akun?</span>
-        <a href="login.php">
-          <span class="text-primary fw-bold"> Login</span>
-        </a>
-      </p>
-
     </div>
   </div>
-  <!-- /Register -->
-</div>
-</div>
-</div>
 
-<!-- / Content -->
-<?php 
+  <!-- / Content -->
+  <?php
 
-include('includes/scripts.php');
-include('includes/footer.php'); 
+  include('includes/scripts.php');
+  include('includes/footer.php');
 
-?>
+  ?>
