00e6ce3e0d9c4f63692ac3d7277da4c390d5ea51
theniswara <daniswara.mdr@proton.me>
2025-06-15 19:26:09 +0700

    Menambahkan tamplan total produk, pelanggan dan transaksi di halaman dashboard

--- admin/index.php
+++ admin/index.php
@@ -6,8 +6,20 @@ if (!isset($_SESSION["login"])) { // jika tdak ada session login
   exit;
 }
 include('includes/header.php');
+require_once '../config/dbcon.php';
 
-
+function getCount($tableName)
+{
+  global $conn;
+  $query = "SELECT COUNT(*) as total FROM `$tableName`";
+  $result = mysqli_query($conn, $query);
+  if ($result) {
+    $row = mysqli_fetch_assoc($result);
+    return (int)$row['total'];
+  } else {
+    return 0;
+  }
+}
 ?>
 
 
@@ -104,101 +116,40 @@ include('includes/header.php');
 <div class="content-wrapper">
   <!-- Content -->
   <div class="container-xxl flex-grow-1 container-p-y">
+    <h4 class="fw-bold mb-4 text-primary">Selamat Datang di Dashboard Kasir GadgetIn</h4>
+    <p class="mb-4">Pantau data produk, pelanggan, dan transaksi secara real-time.</p>
     <div class="row">
-      <div class="col-xxl-8 mb-6 order-0">
-        <div class="card">
-          <div class="d-flex align-items-start row">
-            <div class="col-sm-7">
-              <div class="card-body">
-                <h4 class="card-title text-primary mb-3">Selamat Datang di Dashboard 🎉</h4>
-                <p class="mb-6">
-                  You have done 72% more sales today.<br />Check your new badge in your profile.
-                </p>
-
-                <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
-              </div>
-            </div>
-            <div class="col-sm-5 text-center text-sm-left">
-              <div class="card-body pb-0 px-0 px-md-6">
-                <img
-                  src="assets/img/illustrations/man-with-laptop.png"
-                  height="175"
-                  alt="View Badge User" />
-              </div>
-            </div>
+      <div class="col-md-4 mb-4">
+        <div class="card h-100">
+          <div class="card-body text-center">
+            <i class="bx bx-package text-primary mb-2" style="font-size:2rem"></i>
+            <h6 class="mb-1">Total Produk</h6>
+            <h3 class="fw-bold mb-2"><?= getCount('produk') ?></h3>
+            <a href="list-produk.php" class="btn btn-sm btn-outline-primary mt-2">Lihat Produk</a>
           </div>
         </div>
       </div>
-      <div class="col-xxl-4 col-lg-12 col-md-4 order-1">
-        <div class="row">
-          <div class="col-lg-6 col-md-12 col-6 mb-6">
-            <div class="card h-100">
-              <div class="card-body">
-                <div class="card-title d-flex align-items-start justify-content-between mb-4">
-                  <div class="avatar flex-shrink-0">
-                    <img
-                      src="assets/img/icons/unicons/chart-success.png"
-                      alt="chart success"
-                      class="rounded" />
-                  </div>
-                  <div class="dropdown">
-                    <button
-                      class="btn p-0"
-                      type="button"
-                      id="cardOpt3"
-                      data-bs-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false">
-                      <i class="icon-base bx bx-dots-vertical-rounded text-body-secondary"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
-                      <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                      <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                    </div>
-                  </div>
-                </div>
-                <p class="mb-1">Profit</p>
-                <h4 class="card-title mb-3">$12,628</h4>
-                <small class="text-success fw-medium"><i class="icon-base bx bx-up-arrow-alt"></i> +72.80%</small>
-              </div>
-            </div>
+      <div class="col-md-4 mb-4">
+        <div class="card h-100">
+          <div class="card-body text-center">
+            <i class="bx bx-user text-success mb-2" style="font-size:2rem"></i>
+            <h6 class="mb-1">Total Pelanggan</h6>
+            <h3 class="fw-bold mb-2"><?= getCount('customer') ?></h3>
+            <a href="list-pelanggan.php" class="btn btn-sm btn-outline-success mt-2">Lihat Pelanggan</a>
           </div>
-          <div class="col-lg-6 col-md-12 col-6 mb-6">
-            <div class="card h-100">
-              <div class="card-body">
-                <div class="card-title d-flex align-items-start justify-content-between mb-4">
-                  <div class="avatar flex-shrink-0">
-                    <img
-                      src="assets/img/icons/unicons/wallet-info.png"
-                      alt="wallet info"
-                      class="rounded" />
-                  </div>
-                  <div class="dropdown">
-                    <button
-                      class="btn p-0"
-                      type="button"
-                      id="cardOpt6"
-                      data-bs-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false">
-                      <i class="icon-base bx bx-dots-vertical-rounded text-body-secondary"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt6">
-                      <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                      <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                    </div>
-                  </div>
-                </div>
-                <p class="mb-1">Sales</p>
-                <h4 class="card-title mb-3">$4,679</h4>
-                <small class="text-success fw-medium"><i class="icon-base bx bx-up-arrow-alt"></i> +28.42%</small>
-              </div>
-            </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-4">
+        <div class="card h-100">
+          <div class="card-body text-center">
+            <i class="bx bx-receipt text-warning mb-2" style="font-size:2rem"></i>
+            <h6 class="mb-1">Total Transaksi</h6>
+            <h3 class="fw-bold mb-2"><?= getCount('transaksi') ?></h3>
+            <a href="pesanan.php" class="btn btn-sm btn-outline-warning mt-2">Lihat Pesanan</a>
           </div>
         </div>
       </div>
     </div>
   </div>
   <!-- / Content -->
-
   <?php include('includes/footer.php'); ?>
